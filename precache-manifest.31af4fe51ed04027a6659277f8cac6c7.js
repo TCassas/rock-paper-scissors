f66a9da4ff45a87c34e1456078b5edfdb3643726
Tomás Cassas <tomascassas@gmail.com>
2020-06-19 20:25:48 -0300

    Updates

---
+++ precache-manifest.31af4fe51ed04027a6659277f8cac6c7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8307561dd9075842688a8c85a687c787",
+    "url": "/rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "ca9b62bcb885dafa4ae4",
+    "url": "/rock-paper-scissors/static/css/main.884054dd.chunk.css"
+  },
+  {
+    "revision": "f794be3caa0b6455ee72",
+    "url": "/rock-paper-scissors/static/js/2.0d921330.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rock-paper-scissors/static/js/2.0d921330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca9b62bcb885dafa4ae4",
+    "url": "/rock-paper-scissors/static/js/main.ad4a12e4.chunk.js"
+  },
+  {
+    "revision": "4152367e75813356d958",
+    "url": "/rock-paper-scissors/static/js/runtime-main.9b8d27fd.js"
+  },
+  {
+    "revision": "1fd44bdf9274b331fe659004ccc71e63",
+    "url": "/rock-paper-scissors/static/media/bg-triangle.1fd44bdf.svg"
+  },
+  {
+    "revision": "919ab92a5d671f1b7fdd3e5f97eb816c",
+    "url": "/rock-paper-scissors/static/media/icon-close.919ab92a.svg"
+  },
+  {
+    "revision": "69a8c2e96b659b35a3760c79123be610",
+    "url": "/rock-paper-scissors/static/media/icon-paper.69a8c2e9.svg"
+  },
+  {
+    "revision": "8ce1e109bb43912192f5ba8dfcca005e",
+    "url": "/rock-paper-scissors/static/media/icon-rock.8ce1e109.svg"
+  },
+  {
+    "revision": "bec2f9b3d60cb80987106bb49205c53f",
+    "url": "/rock-paper-scissors/static/media/icon-scissors.bec2f9b3.svg"
+  },
+  {
+    "revision": "5d74d25d9c64bde346585ec7e7587a69",
+    "url": "/rock-paper-scissors/static/media/image-rules.5d74d25d.svg"
+  }
+]);
